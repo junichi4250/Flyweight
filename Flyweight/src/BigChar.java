d06a5752131cbe412ded635b28c0fb17dd1deaf3
junkatou <jkky0225@gmail.com>
2020-12-08 07:39:35 +0900

    Flyweightの実装

---
+++ Flyweight/src/BigChar.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class BigChar {
+	//文字の名前
+	private char charname;
+	//大きな文字を表現する文字列
+	private String fontdata;
+	public BigChar(char charname) {
+		this.charname = charname;
+		try {
+			BufferedReader reader = new BufferedReader(
+					new FileReader("big" + charname + ".txt"));
+			String line;
+			StringBuffer buf = new StringBuffer();
+			while ((line = reader.readLine()) != null) {
+				buf.append(line);
+				buf.append("\n");
+			}
+			reader.close();
+			this.fontdata = buf.toString();
+		} catch (IOException e) {
+			this.fontdata = charname + "?";
+		}
+	}
+
+	public void print() {
+		System.out.println(fontdata);
+	}
+}
